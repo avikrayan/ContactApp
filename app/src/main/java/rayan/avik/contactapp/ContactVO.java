cc5215fca8ea326f3de09757cbfda8049db17618
avik <rayan>
2017-06-21 13:20:15 +0530

    initcommit

---
+++ app/src/main/java/rayan/avik/contactapp/ContactVO.java
@@ -0,0 +1,35 @@
+package rayan.avik.contactapp;
+
+/**
+ * Created by pc41 on 25-04-2017.
+ */
+
+public class ContactVO {
+    private String ContactImage;
+    private String ContactName;
+    private String ContactNumber;
+
+    public String getContactImage() {
+        return ContactImage;
+    }
+
+    public void setContactImage(String contactImage) {
+        ContactImage = contactImage;
+    }
+
+    public String getContactName() {
+        return ContactName;
+    }
+
+    public void setContactName(String contactName) {
+        ContactName = contactName;
+    }
+
+    public String getContactNumber() {
+        return ContactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        ContactNumber = contactNumber;
+    }
+}
---
+++ app/src/main/java/rayan/avik/contactapp/MainActivity.java
@@ -0,0 +1,76 @@
+package rayan.avik.contactapp;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rvContacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        getAllContacts();
+    }
+
+    private void getAllContacts() {
+
+        List<ContactVO> contactVOList = new ArrayList();
+        ContactVO contactVO;
+
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    contactVO = new ContactVO();
+                    contactVO.setContactName(name);
+
+                    Cursor phoneCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id},
+                            null);
+                    if (phoneCursor.moveToNext()) {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contactVO.setContactNumber(phoneNumber);
+                    }
+
+                    phoneCursor.close();
+
+                    Cursor emailCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (emailCursor.moveToNext()) {
+                        String emailId = emailCursor.getString(emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                    }
+                    contactVOList.add(contactVO);
+                }
+            }
+
+            AllContactsAdapter contactAdapter = new AllContactsAdapter(contactVOList, getApplicationContext());
+            rvContacts.setLayoutManager(new LinearLayoutManager(this));
+            rvContacts.setAdapter(contactAdapter);
+        }
+
+    }
+}
